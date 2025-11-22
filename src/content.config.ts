6fa18ca402f301a9f34abcb34c514273912dc039
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-22 17:08:44 +0100

    Added doc pages logic

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,1 +1,10 @@
-export const collections = {};
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const docsPages = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./docs" }),
+});
+
+export const collections = {
+  docsPages,
+};
